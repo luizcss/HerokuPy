1ef43a2bd9fb6991a29e551f6d758480c1f6d70a
luizcss <lzcdoss@gmail.com>
2021-02-18 02:51:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ app = Flask(__name__)
 def nao_entre_em_panico():
     if request.headers.get('Authorization') == '42':
         return jsonify({"42": "a resposta para a vida, o universo e tudo mais"})
-    return jsonify({"message": "Não entre em pânico!"})
+    #return jsonify({"message": "Não entre em pânico!"})
+    return render_template('index.html')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
